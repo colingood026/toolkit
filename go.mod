e6aebbb9276e22abbd2035065e65bfcec56d5cd5
Colin <colin@ColindeMacBook-Air.local>
2023-01-08 15:31:59 +0800

    Initial entry

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/colingood026/toolkit
+
+go 1.19
---
+++ tools_test.go
@@ -0,0 +1,15 @@
+package toolkit
+
+import "testing"
+
+func TestTools_Randomstring(t *testing.T) {
+
+	var testTools Tools
+
+	s := testTools.RandomString(10)
+
+	if len(s) != 10 {
+		t.Error("wrong length random string returned")
+	}
+
+}
